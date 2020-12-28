b6da513ccb2152f3742ced55acbf42b17d72d361
Iain R. Learmonth <irl@Iains-MacBook.local>
2020-12-28 12:15:57 +0000

    buckfast

---
+++ scot_scot.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="scot_scot" tilewidth="32" tileheight="32" tilecount="1" columns="1">
+ <image source="tiles_png/scot_scot.png" width="48" height="48"/>
+</tileset>
